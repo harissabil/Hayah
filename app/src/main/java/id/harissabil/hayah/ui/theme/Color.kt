9d1da8eb816cd08e620cbaef09d12e08189da9c9
Haris <muhammad.haris@visiongroup.co>
2026-04-04 05:18:04 +0700

    Update color definitions for primary, secondary, and tertiary themes

--- app/src/main/java/id/harissabil/hayah/ui/theme/Color.kt
+++ app/src/main/java/id/harissabil/hayah/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package id.harissabil.hayah.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF006D36)
+val PrimaryContainer = Color(0xFF50C878)
+val Secondary = Color(0xFF006A6A)
+val SecondaryContainer = Color(0xFF90EFEF)
+val Tertiary = Color(0xFF8D4F11)
+val TertiaryContainer = Color(0xFFEFA05C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Surface = Color(0xFFFBF9F5)
+val SurfaceContainerLowest = Color(0xFFFFFFFF)
+val SurfaceContainerLow = Color(0xFFF5F3EF)
+val SurfaceContainerHighest = Color(0xFFE4E2DE)
+val OnSurface = Color(0xFF1B1C1A)
+val OutlineVariant = Color(0xFFBDCABC)
+val InverseOnSurface = Color(0xFFF2F0ED)
