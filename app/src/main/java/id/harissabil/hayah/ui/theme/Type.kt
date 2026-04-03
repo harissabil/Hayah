1d96dd379107e2f10a1d2cf63db248a7020782aa
Haris <muhammad.haris@visiongroup.co>
2026-04-04 05:18:14 +0700

    Add custom font families and update typography styles

--- app/src/main/java/id/harissabil/hayah/ui/theme/Type.kt
+++ app/src/main/java/id/harissabil/hayah/ui/theme/Type.kt
@@ -4,31 +4,53 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import id.harissabil.hayah.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val ManropeFont = GoogleFont("Manrope")
+val JakartaFont = GoogleFont("Plus Jakarta Sans")
+
+val ManropeFamily = FontFamily(
+    Font(googleFont = ManropeFont, fontProvider = provider, weight = FontWeight.Normal),
+    Font(googleFont = ManropeFont, fontProvider = provider, weight = FontWeight.Bold)
+)
+
+val JakartaFamily = FontFamily(
+    Font(googleFont = JakartaFont, fontProvider = provider, weight = FontWeight.Normal),
+    Font(googleFont = JakartaFont, fontProvider = provider, weight = FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = ManropeFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 56.sp,
+        letterSpacing = (-1.12).sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = ManropeFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = JakartaFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = JakartaFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
